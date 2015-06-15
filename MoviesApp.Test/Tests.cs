4e758994def6545d1fa8d1ee1050dfe1db66e576
imadhash <immaad@gmail.com>
2015-06-15 12:53:55 +1000

    added a test case

---
+++ MoviesApp.Test/Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieApp.Manager;
+
+namespace MoviesApp.Test
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void CanReadAllMovies()
+        {
+            // just a sample test case
+
+            var movies = MovieManager.Instance.GetAll();
+            Assert.IsNotNull(movies);
+            Assert.AreEqual(80, movies.Count());
+        }
+    }
+}
